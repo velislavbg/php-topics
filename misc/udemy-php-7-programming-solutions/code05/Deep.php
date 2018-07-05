c97e725c224250f4b64efd221e9dda1e5b2aa4a5
af25 <af25@af25.xxxx.net>
2018-07-06 02:02:37 +0300

    5 deep scan get domains fron hrefs

---
+++ misc/udemy-php-7-programming-solutions/code05/Deep.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Web;
+
+class Deep
+{
+	protected $domain;
+
+    /**
+     * Return DNS domain from URL
+     * 
+     * @param string $url - wesite
+     * @return string $dns - domain
+     */
+     public function scan($url, $tag)
+     {
+		 $vac = new Hoover();
+		 $scan = $vac->getAttribute($url, 'href', $this->getDomain($url));
+		 $resutl = array();
+		 foreach($scan as $subSite) {
+		    yield from $vac->getTags($subSite, $tag);
+		 }
+		 // total number of sub-sites found
+		 return count($scan);
+	 }
+}
