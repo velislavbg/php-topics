4677f487d4a340cb2b99d795ec6b6608ec441d0a
af25 <af25@af25.xxxx.net>
2018-07-06 00:42:20 +0300

    autoloader method autoLoad

--- misc/udemy-php-7-programming-solutions/Loader.php
+++ misc/udemy-php-7-programming-solutions/Loader.php
@@ -10,6 +10,25 @@ class Loader
    protected static $dirs = array();
    protected static $registered = 0;
    
+   
+   public function static function autoLoad($class)
+   {
+       $success = FALSE;
+       $fn = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+       foreach(self::$dirs as $start) {
+	      $file = $start . DIRECTORY_SEPARATOR . $fn;
+	      if (self::loadFile($file)){
+		     $success = TRUE;
+		     break;
+		  }
+	   }
+	   if (!success) {
+	      if (!self::loadFile(__DIR__ . DIRECTORY_SEPARATOR . $fn)) {
+		      throw new \Exception(self::UNABLE_TO_LOAD . ' ' . $class);
+		  }
+	   }  
+   }
+   
    protected static function loadFile($file)
    {
        if (file_exists($file)) {
